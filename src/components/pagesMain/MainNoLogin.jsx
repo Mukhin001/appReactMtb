44e1e9624b4e844384d96629a5e08043cb1cf790
Mukhin001 <purgen.igor@yandex.ru>
2024-06-27 16:05:09 +0300

    login3

--- src/components/pagesMain/MainNoLogin.jsx
+++ src/components/pagesMain/MainNoLogin.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
+
 const MainNoLogin = () => {
     const [createUserName, getCreateUserName] = useState('');
     const [createUserEmail, getcreateUserEmail] = useState('');
     const [createPassowordOne, getCreatePassowordOne] = useState('');    
     const [createPassowordTwo, getCreatePassowordTwo] = useState(''); 
+
 
     function getUserNameFn(e) {
         getCreateUserName(e.target.value);
@@ -27,18 +29,20 @@ const MainNoLogin = () => {
         if(e.target.value.length >= 6 && strElem.length >= 3) {
             getCreatePassowordOne(e.target.value);
             alert('good password.');
-        }
+        } 
         
     };
-
+ 
     function getUserpasswordTwoFn(e) {
         getCreatePassowordTwo(e.target.value);
     };
 
     function createAccauntFn() {
         const EMAIL = ['@mail', '@yandex', '@google', '@rambler'];
-
-        if(createPassowordOne === createPassowordTwo) {
+        
+        if(createPassowordOne < 5) {
+            alert('rait > 6 word');
+        } else if(createPassowordOne === createPassowordTwo) {
             console.log(createUserName, createPassowordOne);
             localStorage.setItem(createUserName, createPassowordOne)
         } else {
@@ -58,24 +62,37 @@ const MainNoLogin = () => {
 
     return ( 
         <div>
-            <form action="">
+            <form action="" method="post" name="userRegistered">
                 <div>
                     <label htmlFor="login">login</label>
-                    <input type="text" id="login"  onChange={getUserNameFn}/>
+                    <input type="text" id="login" name="login" placeholder="login" onChange={getUserNameFn} required/>
                 </div>
                 <div>
                     <label htmlFor="email">email</label>
-                    <input type="email" id="email" onChange={getUserEmail}/>
+                    <input type="email" id="email" name="email" placeholder="@email" onChange={getUserEmail} required/>
                 </div>
+                <fieldset>
+                    <legend>gender...</legend>
+                    <input type="radio" id="Male" name="gender" defaultChecked/><label htmlFor="Male">Male</label>
+                    <input type="radio" id="Female" name="gender"/><label htmlFor="Female">Female</label>
+                </fieldset>
+                <fieldset>
+                    <legend>Choose your interests...</legend>
+                    <input type="checkbox" id="Coding" name="checkbox" value="checkbox-empty"/><label htmlFor="Coding">Coding</label>
+                    <input type="checkbox" id="Music" name="checkbox" value="checkbox-empty"/><label htmlFor="Music">Music</label>
+                    <input type="checkbox" id="Sport" name="checkbox" value="checkbox-empty"/><label htmlFor="Sport">Sport</label>
+                    <input type="checkbox" id="Movie" name="checkbox" value="checkbox-empty"/><label htmlFor="Movie">Movie</label>
+                </fieldset>
                 <div>
                     <label htmlFor="password1">password</label>
-                    <input type="password" id="password1" onChange={getUserpasswordOneFn}/>
+                    <input type="password" id="password1" name="password1" placeholder="passowd" onChange={getUserpasswordOneFn} required/>
                 </div>
                 <div>
                     <label htmlFor="password2">password</label>
-                    <input type="password" id="password2" onChange={getUserpasswordTwoFn}/>
+                    <input type="password" id="password2" name="password2" placeholder="passowd" onChange={getUserpasswordTwoFn} required/>
                 </div>
-                <button onClick={createAccauntFn}>Create accaunt</button>
+                <input type="submit" onClick={createAccauntFn} value="Create accaunt"/>
+                <input type="reset" value="reset" />
             </form>
         </div>
      );
