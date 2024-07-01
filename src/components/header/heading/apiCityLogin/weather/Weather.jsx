6ca3d50b65b6a3cee9ebb2680ab604eb474d5300
Mukhin001 <purgen.igor@yandex.ru>
2024-07-01 20:43:30 +0300

    01.07home

--- src/components/header/heading/apiCityLogin/weather/Weather.jsx
+++ src/components/header/heading/apiCityLogin/weather/Weather.jsx
@@ -1,4 +1,26 @@
+import { joinPaths } from "@remix-run/router";
+import { json } from "react-router-dom";
+
 const Weather = () => {
+    const city = 'moscow';
+    const key = '424138bd1df061d0694b6ebce42f3ff7';
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+    // const promise = new Promise(function(resolve, reject) {
+
+    //     resolve(api);
+    //     reject(console.log('error'))
+    // });
+    // promise.then((data) => {
+    //    return json(data);
+    // }).then((data) => {
+    //     console.log(json(data));
+    // });
+    const weatherApi = fetch(api);
+    weatherApi.then((data) => {
+       return data.json()
+    }).then((data) => {
+        console.log(data);
+    })
     return ( 
         <div>
             Weather
--- src/components/pagesMain/MainLogin.jsx
+++ src/components/pagesMain/MainLogin.jsx
@@ -14,27 +14,32 @@ const MainLogin = ({enterAcc}) => {
     };
     
     function checkUserAccaunt(e) {
-            
-        for(let i = 0; i < localStorage.length; i++) {
-            let key = localStorage.key(i);
-
-            if(key === login && localStorage.getItem(key) === password)  {
-                enterAcc(key);
-                break;
-            } else if(login === '' || password === '') {
-                alert('empty');
-                e.preventDefault();
-                break;
-            } else if(Object.keys(localStorage).every((e) => e !== login)) {
-                alert('invakid login');
-                e.preventDefault();
-                break;
-            } else if(Object.values(localStorage).every((e) => e !== password)) {
-                alert('invakid password');
-                e.preventDefault();
-                break;
+        if (Object.keys(localStorage).length){
+            for(let i = 0; i < localStorage.length; i++) {
+                let key = localStorage.key(i);
+                    
+                if(key === login && localStorage.getItem(key) === password)  {
+                    enterAcc(key);
+                    break;
+                } else if(login === '' || password === '') {
+                    alert('empty');
+                    e.preventDefault();
+                    break;
+                } else if(Object.keys(localStorage).every((e) => e !== login)) {
+                    alert('invakid login');
+                    e.preventDefault();
+                    break;
+                } else if(Object.values(localStorage).every((e) => e !== password)) {
+                    alert('invakid password');
+                    e.preventDefault();
+                    break;
+                } 
             }
+        } else {
+            alert('localStorage empty create accaunt please.');
+            e.preventDefault();
         }
+
     };
 
     return ( 
