c53a7bb9a895539e1e8d9e97e86092a2bd3be17f
Mukhin001 <purgen.igor@yandex.ru>
2024-07-24 21:29:34 +0300

    24.07home

--- src/components/pagesMain/mainPhoto/pagesPhoto/PagePhotos.jsx
+++ src/components/pagesMain/mainPhoto/pagesPhoto/PagePhotos.jsx
@@ -1,38 +1,65 @@
 import { NavLink } from "react-router-dom";
 import st from './style.module.css';
 import { photoServer } from './photoServer';
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 
 
 const PagePhotos = ({getLikesFn}) => {
-    const imgRef = useRef();
-
+    const imgRef = useRef(null);
+    const slideDots = useRef(null);
+    const [arrImg, setArrImg] = useState([]);
+ 
     function getLikes(e) {
         getLikesFn(e.target.parentNode.parentNode);
     };
 
     function mouseOver() {
-         //imgRef.current.style.display = 'flex';
-        console.log(imgRef.current);
+        // [...imgRef.current.children].forEach(li => {
+        // //     [...li.children[0].children[0].children].slice(0, -1).forEach(img => img.style.display = 'none');
+        // //    // console.log([...li.children[0].children[0].children]);
+        // //     [...li.children[0].children[0].children][0].style.display = 'block';
+        // });
+        // console.log(1);
+       
+        [...imgRef.current.children].forEach(li => {
+            let dots = ([...li.children[0].children[0].children].slice(-1));
+            //[...li.children[0].children[0].children].slice(0, -1).forEach(img => img.style.display = 'none');
+
+            for(let i = 0; i < dots.length; i++) {
+                
+        // //    // console.log([...li.children[0].children[0].children]);
+          //  [...li.children[0].children[0].children][i].style.display = 'block';
+                for(let j = 0; j < [...li.children[0].children[0].children].length; j++) {
+                     [...li.children[0].children[0].children][i].style.display = 'none';
+                }
+                //console.log( [...li.children[0].children[0].children]);
+            }
+        });
     };
 
-   
+   useEffect(() => {
+        [...imgRef.current.children].forEach(li => {
+         
+            [...li.children[0].children[0].children][0].style.display = 'block';
+            ([...li.children[0].children[0].children].slice(-1));
+        });
+       // setArrImg(imgRef.current.children);
+       
+   });
 
     return ( 
         <div className={st.pagePhotosWrap}>
-            <ul>
+            <ul ref={imgRef}>
                 {photoServer.map((obj) => {
                     
                     return (
                         <li atrlike={obj.name} namephoto={obj.name} className={st.photosImgLi} key={obj.name}>
-                            <div style={{position: 'relative'}}  onMouseOver={mouseOver}  >
+                            <div style={{position: 'relative'}}  onMouseOver={mouseOver} >
                                 <NavLink to={`/photo/${obj.name}`}>
-                                {/* <img src={obj.url[0]} alt={obj.name} className={st.imgPhotos}/> */}
+                               
                                 {obj.url.map(url => {
-                                    
-                                    // url[0].className={st.imgActiveSlide}
-                                    return ( <img ref={imgRef} style={{display: 'none'}}  src={url} key={url} alt={url.substring(26)}/>)})}
-                                <div  className={st.gridHoverImg} style={{gridTemplateColumns: `repeat(${obj.url.length}, 1fr)`}}>
+                                    return ( <img src={url} key={url} alt={url.substring(26)}  />)})}
+                                <div ref={slideDots} className={st.gridHoverImg} style={{gridTemplateColumns: `repeat(${obj.url.length}, 1fr)`}}>
                                     {obj.url.map((url, i) => {
                                         return (<div className={st.slideTods} key={url.substring(2)}>{i}</div>)
                                     })}
