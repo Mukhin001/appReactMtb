37c0dd5fc38087ae99077e0612901c0fa0d99a4c
Mukhin001 <purgen.igor@yandex.ru>
2024-09-26 22:03:26 +0300

    26.09work

--- src/components/pagesMain/mainHome/sliderHome/carouselHome/CarouselHome.jsx
+++ src/components/pagesMain/mainHome/sliderHome/carouselHome/CarouselHome.jsx
@@ -1,35 +1,58 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { imgArrServerCarousel } from '../../../../../server/imgArrServer';
 import st from './style.module.css';
 
 const CarouselHome = () => {
-    const carouselArr = imgArrServerCarousel.slice();
+    const carouselArr = imgArrServerCarousel.slice(0, 5);
+    const arrLength = carouselArr.length;
     const [left, setLeft] = useState(0);
     const [arr, setArr] = useState(carouselArr);
     const ulRef = useRef();
-    const [widthUl, setWidthUl] = useState();
+    //const [widthUl, setWidthUl] = useState();
+    const [currentClick, setCurrentClick] = useState(0);
+    const [currentArr, setCurrentArr] = useState(0);
     
-    useEffect(() => {
-        const total = [...ulRef.current.children].reduce((acc, e) =>   {
-            return  acc  + e.offsetWidth;
+    // useEffect(() => {
+    //     const total = [...ulRef.current.children].reduce((acc, e) =>   {
+    //         return  acc  + e.offsetWidth;
             
-        } , 0);
+    //     } , 0);
 
-        setWidthUl(total);
-    }, []);
+    //     setWidthUl(total);
+    // }, []);
 
     function btnLeftClick() {
         setLeft(prev => prev + -100);
-        
-        arr.push(arr.shift());
+        setCurrentClick(prev => prev - 1);
+        setCurrentArr(prev => prev + 5);
 
-            if(left < -widthUl / 2) {
-                setLeft(0)
-            }
+        if(currentClick === 0) {
+            setArr(prev => [...prev, ...arr]);
             
-        setArr(arr);
+        } else if(currentClick === -arrLength) {
+            
+            arr.forEach((e, i, a) => a.shift());
+            console.log(arr);
+            
+            setArr( arr );
+            setCurrentClick(0);
+            console.log(true);
+            setLeft(prev => prev + 500);
+        }
+
+        
+        
     };
 
+    function btnRightClick() {
+        setCurrentClick(prev => prev + 1);
+        setLeft(prev => prev + 100);
+       
+        setArr(prev => [...prev, ...arr]);
+            
+        console.log(arr);
+        
+    }
 
 
     return ( 
@@ -37,13 +60,13 @@ const CarouselHome = () => {
             <div  className={st.wrapperBtnSliderClickHome}>
                 <button onClick={btnLeftClick}>{' < left '}</button>
                 
-                <button >{' right > '}</button>
+                <button onClick={btnRightClick} >{' right > '}</button>
             </div>
             <div className={st.sliderMouseHome}>
-                <ul ref={ulRef} className={st.ulWrapperImg} style={{left: `${left}px`}}>
-                    {arr.map(img => {
+                <ul ref={ulRef} className={st.ulWrapperImg} style={{ transition: 'ease 1.5s', left: `${left}px`,}}>
+                    {arr.map((img, i) => {
                         return (
-                            <li className={st.wrapperImg} key={img}>
+                            <li className={st.wrapperImg} key={img + i}>
                                 <img src={`./img/Стикеры/${img}`} alt={img} /> 
                                 <span>{'<>'}</span>       
                             </li>
